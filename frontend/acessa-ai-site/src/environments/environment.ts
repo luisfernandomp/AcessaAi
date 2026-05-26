1b1103f7b5aabb3d102ed2f876faf9cac6a6526d
Luis Fernando <luisfernando20140@outlook.com>
2026-05-26 14:53:39 -0300

    up

--- frontend/acessa-ai-site/src/environments/environment.ts
+++ frontend/acessa-ai-site/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://98.85.166.70/api',
+  apiUrl: 'http://98.85.166.70:5000/api',
   // Obtenha sua chave em: https://console.cloud.google.com → APIs → Maps JavaScript API
   googleMapsApiKey: 'AIzaSyDc6aEhToLRWlNXDfN8fETiyIRbPVq3ZfY',
   // Crie um Map ID em: https://console.cloud.google.com → Google Maps → Map Management
