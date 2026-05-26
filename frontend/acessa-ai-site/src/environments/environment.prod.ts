543fc067c2b6d363134e814ccacb819428f6a3a9
Luis Fernando <luisfernando20140@outlook.com>
2026-05-26 19:47:43 -0300

    up

--- frontend/acessa-ai-site/src/environments/environment.prod.ts
+++ frontend/acessa-ai-site/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://acessaai2002.duckdns.org/api',
-  googleMapsApiKey: '',
-  googleMapsMapId: 'DEMO_MAP_ID',
+  googleMapsApiKey: 'AIzaSyDc6aEhToLRWlNXDfN8fETiyIRbPVq3ZfY',
+  googleMapsMapId: 'f422f359119ba6d8840bb786',
 };
