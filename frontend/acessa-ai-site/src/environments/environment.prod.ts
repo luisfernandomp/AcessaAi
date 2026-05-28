dbc451e8dc98de35bfe4abf348d93d0761a6e68d
Luis Fernando <luisfernando20140@outlook.com>
2026-05-28 14:01:32 -0300

    up

--- frontend/acessa-ai-site/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://acessaai2002.duckdns.org/api',
-  googleMapsApiKey: 'AIzaSyDc6aEhToLRWlNXDfN8fETiyIRbPVq3ZfY',
-  googleMapsMapId: 'f422f359119ba6d8840bb786',
-};
--- frontend/acessa-ai-site/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://acessaai2002.duckdns.org/api',
-  // Obtenha sua chave em: https://console.cloud.google.com → APIs → Maps JavaScript API
-  googleMapsApiKey: 'AIzaSyDc6aEhToLRWlNXDfN8fETiyIRbPVq3ZfY',
-  // Crie um Map ID em: https://console.cloud.google.com → Google Maps → Map Management
-  googleMapsMapId: 'f422f359119ba6d8840bb786',
-};
